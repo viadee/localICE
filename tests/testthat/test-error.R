e71893c00fa20b2df8055c52181629ff699bd050
Martin Walter <mf-walter@web.de>
2019-01-17 15:55:47 +0100

    more tests

--- tests/testthat/test-error.R
+++ tests/testthat/test-error.R
@@ -1,47 +1,6 @@
-context("test error")
+context("test arguments")
 
-test_that("Classification as regression", {
-
-  if(require("randomForest")){
-  rf = randomForest(Species ~., data = iris, ntree = 10)
-
-  expect_error(
-    localICE(
-      instance = iris[3,],
-      data = iris,
-      feature_1 = "Sepal.Length",
-      feature_2 = "Petal.Width",
-      target = "Species",
-      model = rf,
-      regression = T,
-      step_1 = 0.5,
-      step_2 = 0.5
-    )
-  )
-  }
-})
-# test_that("Regression as classification", {
-#
-#   if(require("randomForest")){
-#   rf = randomForest(Petal.Width ~., data = iris, ntree = 10)
-#
-#   expect_error(
-#     localICE(
-#       instance = iris[3,],
-#       data = iris,
-#       feature_1 = "Sepal.Length",
-#       feature_2 = "Species",
-#       target = "Petal.Width",
-#       model = rf,
-#       regression = F,
-#       step_1 = 0.1,
-#       step_2 = 0.1
-#     )
-#   )
-#   }
-#
-# })
-test_that("Classification as regression", {
+test_that("aruments", {
 
   if(require("randomForest")){
     rf = randomForest(Species ~., data = iris, ntree = 10)
